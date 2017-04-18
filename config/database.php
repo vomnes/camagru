7d1524912356205d5784c7832ad3c1f6fc372626
Valentin OMNES <vomnes@student.42.fr>
2017-04-18 20:17:44 +0200

    camagru

---
+++ config/database.php
@@ -0,0 +1,7 @@
+<?php
+    $DB_DSN = 'mysql:host=localhost;dbname=db_camagru';
+    $DB_USER = 'root';
+    $DB_PASSWORD = 'root';
+    $DB_NAME = 'db_camagru';
+    $DB_OPTIONS = array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION);
+?>

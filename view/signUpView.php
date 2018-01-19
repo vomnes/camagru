7a3f23e341e6efd577ff2a1a7df155d8ebd613dc
Valentin OMNES <vomnes@student.42.fr>
2018-01-19 18:37:31 +0100

    feat: Improve css and add message result signup

--- view/signUpView.php
+++ view/signUpView.php
@@ -5,21 +5,22 @@
        <fieldset>
          <label for="name">Username: </label><br><input type="text" name="username"><br>
          <label for="name">Password: </label><br><input type="password" name="password"><br>
+         <label for="name">Re-enter password </label><br><input type="password2" name="password"><br>
          <label for="name">Email address: </label><br><input type="text" name="email"><br>
        </fieldset>
        <p style="text-align: center;"><input class="submit-btm" type="submit"></p>
+       <?php
+       if ($signUpCode == 1) {
+         echo "<p style=\"text-align: center; color: ForestGreen;\">Your account has been created</p>";
+       } else if ($signUpCode == 2) {
+         echo "<p style=\"text-align: center; color: Brown;\">Please choose another login or email address</p>";
+       } else if ($signUpCode == 3) {
+         echo "<p style=\"text-align: center; color: Brown;\">Username must not be empty</p>";
+       } else if ($signUpCode == 4) {
+         echo "<p style=\"text-align: center; color: Brown;\">Password must not be empty</p>";
+       } else if ($signUpCode == 5) {
+         echo "<p style=\"text-align: center; color: Brown;\">Email address must not be empty</p>";
+       }
+       ?>
      </form>
-     <p><?php
-     if ($signUpCode == 1) {
-       echo "Your account has been created";
-     } else if ($signUpCode == 2) {
-       echo "Please choose another login or email address";
-     } else if ($signUpCode == 3) {
-       echo "Username must not be empty";
-     } else if ($signUpCode == 4) {
-       echo "Password must not be empty";
-     } else if ($signUpCode == 5) {
-       echo "Email address must not be empty";
-     }
-     ?></p>
    </div>

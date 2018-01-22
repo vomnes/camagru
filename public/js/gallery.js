3790bee7629c13fca97c1c2013d21a2fd80d7fb7
Valentin OMNES <vomnes@student.42.fr>
2018-01-22 18:57:51 +0100

    feat: Add gallery handle comments and likes

--- public/js/gallery.js
+++ public/js/gallery.js
@@ -21,3 +21,44 @@ function showsDiv(elem) {
     document.getElementById(elem).style.display = "block";
   }
 }
+
+function addComment(index, username) {
+  let message = document.getElementById('content-comment-' + index);
+  if (message.value == "") {
+    let placeholder = message.placeholder
+    message.placeholder = '';
+    setTimeout(function(){
+      message.placeholder = placeholder;
+    }, 100);
+    return;
+  }
+  document.getElementById('comment-list-' + index).innerHTML +=
+  "<div class=\"one-comment\">\n<a class=\"comment-owner\">"+username+"</a>\n<a class=\"comment-text\">"+message.value+"</a>\n</div>";
+  document.getElementById('content-comment-' + index).value = "";
+}
+
+function getNumberLikes(index) {
+  var count = document.getElementById('like-text-'+index).innerHTML;
+  count = parseInt(count);
+  if (isNaN(count)) {
+    return 0;
+  }
+  return count;
+}
+
+function handleLikes(index, elem, src1, src2) {
+  var src = document.getElementById(elem).src;
+  var likes = getNumberLikes(index);
+  src = src.substring(src.indexOf("/public/") + 1);
+  if (src == src1) {
+    document.getElementById(elem).src = src2;
+    document.getElementById('like-text-'+index).innerHTML = '+'+(likes+1);
+  } else {
+    document.getElementById(elem).src = src1;
+    if (likes-1 <= 0) {
+      document.getElementById('like-text-'+index).innerHTML = '';
+    } else {
+      document.getElementById('like-text-'+index).innerHTML = '+'+(likes-1);
+    }
+  }
+}
--- view/galleryView.php
+++ view/galleryView.php
@@ -1,6 +1,6 @@
 <h2 id="title-page">Gallery</h2>
 <!-- One picture of the gallery -->
-<div id="gallery">
+<div class="gallery" id="gallery-1">
   <!-- Header of the picture -->
   <div class="header-picture">
     <div class="thumbnail">
@@ -12,44 +12,32 @@
   <img class="gallery-picture" src="public/pictures/illustration-2.jpg" alt="gallery picture">
   <!-- Footer of the picture -->
   <div class="footer-picture">
-    <img class="like-icon" src="public/pictures/like-black-128.png" alt="like red">
-    <a class="like-text">+100</a>
-    <button class="open-comments" onclick='showsDiv("comments-picture")'>Comments</button>
+    <img class="like-icon" id="like-icon-1" onclick="handleLikes(1, 'like-icon-1', 'public/pictures/like-black-128.png', 'public/pictures/like-red-128.png')" src="public/pictures/like-black-128.png" alt="like red">
+    <a class="like-text" id="like-text-1">7</a>
+    <button class="open-comments" onclick='showsDiv("comments-picture-1")'>Comments</button>
   </div>
   <!-- Comment of the picture -->
-  <div id="comments-picture">
-    <div class="one-comment">
-      <a class="comment-owner">Valentin</a>
-      <a class="comment-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras id hendrerit tortor. Duis rutrum enim nec nisl faucibus, ut laoreet arcu elementum. Nunc elementum porta semper.</a>
-    </div>
-    <div class="one-comment">
-      <a class="comment-owner">Antoine</a>
-      <a class="comment-text">Consectetur adipiscing elit.</a>
-    </div>
-    <div class="one-comment">
-      <a class="comment-owner">Sophie</a>
-      <a class="comment-text">Duis rutrum enim nec nisl faucibus, ut laoreet arcu elementum. Nunc elementum porta semper.</a>
-    </div>
+  <div class="comments-picture" id="comments-picture-1">
+    <div id="comment-list-1">
+        <div class="one-comment">
+          <a class="comment-owner">Valentin</a>
+          <a class="comment-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras id hendrerit tortor. Duis rutrum enim nec nisl faucibus, ut laoreet arcu elementum. Nunc elementum porta semper.</a>
+        </div>
+        <div class="one-comment">
+          <a class="comment-owner">Antoine</a>
+          <a class="comment-text">Consectetur adipiscing elit.</a>
+        </div>
+        <div class="one-comment">
+          <a class="comment-owner">Sophie</a>
+          <a class="comment-text">Duis rutrum enim nec nisl faucibus, ut laoreet arcu elementum. Nunc elementum porta semper.</a>
+        </div>
+      </div>
     <div class="new-comment">
       <fieldset>
-        <textarea type="text" name="comment" class="content-comment" placeholder="Your comment ..." title="Content of your comment" cols="40" rows="5"></textarea>
+        <textarea type="text" name="comment" class="content-comment" id="content-comment-1" placeholder="Your comment ..." title="Content of your comment" cols="40" rows="5"></textarea>
       </fieldset>
-      <p><input class="put-comment" type="submit" value=""></p>
+      <p><input class="put-comment" type="submit" value="" onclick="addComment(1, 'username')"></p>
     </div>
   </div>
 </div>
 <!-- Done -->
-<div id="gallery">
-  <div class="header-picture">
-    <div class="thumbnail">
-      <img src="public/pictures/profile-picture.jpg" alt="gallery profile picture">
-    </div>
-    <a class="gallery-username">Valentin</a>
-  </div>
-  <img class="gallery-picture" src="public/pictures/illustration-1.jpg" alt="gallery picture">
-  <div class="footer-picture">
-    <img class="like-icon" src="public/pictures/like-black-128.png" alt="like red">
-    <a class="like-text">+100</a>
-    <a class="comments-text">Comments</a>
-  </div>
-</div>
